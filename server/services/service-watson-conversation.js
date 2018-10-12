207fb59432c48a789038439c86faa411f4c8b226
Mike Rheinheimer <mrheinheimer@ohmyhead.com>
2018-10-12 14:55:56 -0500

    Update service-watson-conversation.js

--- server/services/service-watson-conversation.js
+++ server/services/service-watson-conversation.js
@@ -4,8 +4,8 @@ const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 module.exports = function(app, serviceManager){
 	const assistant = new AssistantV1({
 		iam_apikey: process.env.WATSON_ASSISTANT_APIKEY,
-		username: process.env.WATSON_ASSISTANT_USERNAME,
-		password: process.env.WATSON_ASSISTANT_PASSWORD,
+		//username: process.env.WATSON_ASSISTANT_USERNAME,
+		//password: process.env.WATSON_ASSISTANT_PASSWORD,
 		url: process.env.WATSON_ASSISTANT_URL,
 		version: '2018-09-20',
 	});
