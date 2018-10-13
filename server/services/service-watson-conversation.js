8a3658085173c01e33f3065130c0d34cee13d59e
Andrew Trice <amtrice@us.ibm.com@Andrews-MacBook-Pro.local>
2018-10-12 23:00:27 -0400

    updated mappings

--- server/services/service-watson-conversation.js
+++ server/services/service-watson-conversation.js
@@ -3,10 +3,8 @@ const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 
 module.exports = function(app, serviceManager){
 	const assistant = new AssistantV1({
-		iam_apikey: IBMCloudEnv.getString('watson_conversation_apikey'),
-		username: IBMCloudEnv.getString('watson_conversation_username'),
-		password: IBMCloudEnv.getString('watson_conversation_password'),
-		url: IBMCloudEnv.getString('watson_conversation_url'),
+		iam_apikey: IBMCloudEnv.getString('watson_assistant_apikey'),
+		url: IBMCloudEnv.getString('watson_assistant_url'),
 		version: '2018-07-10',
 	});
 
